264b80f1cd0dfb6a89e57139609ce74dd6fd822d
Stephen Anderson <stephenandersondev@gmail.com>
2020-10-20 12:15:47 -0500

    Add files via upload

---
+++ frontend/scripts/game.js
@@ -0,0 +1,11 @@
+class Game {
+    constructor(player) {
+        this.player = player
+        this.score = 0
+    }
+    test = () => {
+        ++this.score
+        console.log(this.player, this.score)
+    }
+    
+}
