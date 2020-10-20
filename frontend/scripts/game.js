46cd3928ca2949e5840ad828927110444e32842b
stephenandersondev <stephenandersondev@gmail.com>
2020-10-20 17:58:01 -0500

    added monkey to game

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -4,8 +4,11 @@ class GameSession {
     }
 }
 
-let canvas = document.getElementById("game-canvas")
-let ctx = canvas.getContext("2d")
+const canvas = document.getElementById("game-canvas")
+const ctx = canvas.getContext("2d")
+const GAME_WIDTH = 750
+const GAME_HEIGHT = 900
+ctx.clearRect(0, 0, 750, 900)
 
 class Game {
     constructor() {
@@ -18,3 +21,27 @@ startBtn.addEventListener("click", (e) => {
     let gameInstance = new Game()
     console.log(gameInstance.score)
 })
+
+
+class Monkey {
+
+    constructor(gameWidth, gameHeight) {
+        this.image = new Image()
+        this.image.src = 'assets/img/game-img/monkey.png';
+        this.image.width = 120
+        this.image.height = 120
+        this.image.position = {
+            x: gameWidth / 2 - this.image.width / 2,
+            y: gameHeight - this.image.height - 10
+        }
+    }
+
+    draw = (ctx) => {
+        ctx.drawImage(this.image, this.image.position.x, this.image.position.y, this.image.width, this.image.height)
+    }
+}
+
+let monkey = new Monkey(GAME_WIDTH, GAME_HEIGHT)
+window.onload = function() {
+    monkey.draw(ctx)
+}
